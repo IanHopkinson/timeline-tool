ef8c7accbc31fc5638f6aa84f59246734f7dbcbf
Ian Hopkinson <ian@scraperwiki.com>
2013-04-10 19:41:34 +0100

    User interface part-complete

--- TimeLineScript.js
+++ TimeLineScript.js
@@ -1,6 +1,50 @@
 var tl;
 var GlobalData
-        function onLoad() {
+var TargetTable
+var TitleField
+var StartField
+var EndField
+var ColourField
+
+
+		function onLoad() {
+			// Get the table data using a metarequest
+			TargetTable = $('select[name="TargetTable"]');
+			TitleField = $('select[name="TitleField"]');
+			StartField = $('select[name="StartField"]');
+			EndField = $('select[name="EndField"]');
+			ColourField = $('select[name="ColourField"]');
+
+			scraperwiki.sql.meta(function(metadata, textStatus, jqXHR) {
+			for(tableName in metadata.table){
+				console.log('table:', tableName, 'columns:', metadata.table[tableName].columnNames)
+			}
+			// Populate table options in the form with data from query
+			// TODO - bug here that we always select the last one
+				
+				$.each(metadata.table, function(name) {
+				//console.log(name)
+				TargetTable.append($("<option />").val(name).text(name));
+				});
+			// Populate fields tables
+
+				console.log(metadata.table[tableName].columnNames[0])
+				for (i=0;i<metadata.table[tableName].columnNames.length;i++) {
+				var name=metadata.table[tableName].columnNames[i]
+				TitleField.append($("<option />").val(name).text(name));
+				StartField.append($("<option />").val(name).text(name));
+				EndField.append($("<option />").val(name).text(name));
+				ColourField.append($("<option />").val(name).text(name));
+				};
+				
+			}, function(jqXHR, textStatus, errorThrown){
+				console.log('Oh no! Error:', jqXHR.responseText, textStatus, errorThrown)
+			})
+			
+
+		}
+		
+        function showTimelineFunction() {
             var eventSource = new Timeline.DefaultEventSource(0);
             
             // Example of changing the theme from the defaults
@@ -43,8 +87,12 @@ var GlobalData
 			// Copied from the ScraperWiki docs:
 			//  https://code.google.com/p/simile-widgets/wiki/Timeline_EventSources
 			
+			// Get data from form
+			console.log(TargetTable.val())
+			console.log(TitleField.val())
+			
 			scraperwiki.sql("select * from Sheet1", function(data, textStatus, jqXHR) {
-				console.log('Great! Here is your war timeline data:', data);
+				//console.log('Great! Here is your war timeline data:', data);
 				GlobalData=data
 				DBaseOutput=
 				{
@@ -52,13 +100,17 @@ var GlobalData
 					'events': []
 				}
 				var i
-				for (i=0;i<GlobalData.length;i++){ //GlobalData.length
-					var e={start:GlobalData[i].Start.toString(),end:GlobalData[i].End.toString(),title:GlobalData[i].War,color:GlobalData[i].ColourCode }
-					DBaseOutput.events[i]=e
+				for (i=0; i<GlobalData.length; i++){ //GlobalData.length
+					var e = {
+					  start:GlobalData[i].Start.toString(),
+					  end:GlobalData[i].End.toString(),
+					  title:GlobalData[i].War,color:GlobalData[i].ColourCode
+					}
+					DBaseOutput.events[i] = e
 				} 
 				//DBaseOutputJSON=JSON.stringify(DBaseOutput)
 				//This doesn't work:
-				eventSource.loadJSON(DBaseOutput,"." ); //document.location.href
+				eventSource.loadJSON(DBaseOutput, "." ); //document.location.href
 				// This works:
 				//tl.loadJSON("WarsData.js?"+ (new Date().getTime()), function(json, url) {eventSource.loadJSON(json, url);});
 			
